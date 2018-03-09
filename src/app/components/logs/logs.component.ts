1b4d36c79c7f82988160f4a3dfe097be270a9550
shlomi cohen <shlommi@gmail.com>
2018-03-09 12:02:12 +0200

    adding more Observables

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -13,7 +13,9 @@ export class LogsComponent implements OnInit {
   constructor(private logService: LogService ) { }
 
   ngOnInit() {
-    this.logs = this.logService.getLogs();
+    this.logService.getLogs().subscribe(logs => {
+      this.logs = logs;
+    });
   }
 
   onSelect(log: Log) {
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -34,8 +34,8 @@ export class LogService {
     ];
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
   }
 
   setFormLog(log: Log) {
